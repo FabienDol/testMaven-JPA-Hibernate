48f5b24527c80918bb6195137ac0da55f1827feb
FabienD <phwright26@gmail.com>
2022-04-08 12:57:33 +0200

    added Employee entity

--- src/main/java/com/cgi/Main.java
+++ src/main/java/com/cgi/Main.java
@@ -1,5 +1,7 @@
 package com.cgi;
 
+import com.cgi.model.Employee;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
@@ -8,6 +10,13 @@ public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("company");
+        EntityManager em = emf.createEntityManager();
+
+        // crée un employé en db
+        /*Employee e1 = new Employee("Frodon", "Sacquet", 5000);
+        em.getTransaction().begin();
+        em.persist(e1);
+        em.getTransaction().commit();*/
 
         emf.close();
 
